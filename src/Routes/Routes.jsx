d695b194be5b1dbdc284122515b5184d0edc3169
Afroz Faria <afroz_faria03@yahoo.com>
2024-11-02 01:12:27 +0600

    react-icons installed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,46 +1,9 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
-import Main from "../Layout/Main";
-import Home from "../pages/Home/Home";
-import Shop from "../pages/Shop/Shop";
-import Login from "../pages/login/login";
-import SignUp from "../pages/signUp/SignUp";
 import Dashboard from "../Layout/Dashboard/Dashboard";
-import Cart from "../Dashboard_Pages/Cart";
-import Blogs from "../pages/Blogs/Blogs";
-// import PrivateRoute from "./PrivateRoute";
 
   export const router = createBrowserRouter([
-    // {
-    //   path: "/",
-    //   element: <Main></Main>,
-    //   children:[
-    //     {
-    //         path: '/',
-    //         element: <Home></Home> ,
-    //     },
-    //     {
-    //         path: '/shop/:category',
-    //         element: <Shop></Shop>,
-    //         // element: <PrivateRoute>
-    //         //            <Shop></Shop>
-    //         //         </PrivateRoute> ,
-    //     },
-    //     {
-    //       path: '/blogs',
-    //       element: <Blogs></Blogs> ,
-    //     },
-    //     {
-    //         path: '/login',
-    //         element: <Login></Login>,
-    //     },
-    //     {
-    //         path: '/signUp',
-    //         element: <SignUp></SignUp>,
-    //     },
-    //   ]
-    // },
     {
       path: "/dashboard",
       element: <Dashboard></Dashboard>,
